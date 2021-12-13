87d2bb829088bc868d81431adc9b1d1262be0ed7
brylimz <superuser@brylimz.com>
2021-12-13 17:43:20 +0700

    best programming language

--- main.py
+++ main.py
@@ -10,3 +10,5 @@ with open("input/letters/starting_letter.txt") as letter_file:
         stripped_name = name.strip()
         new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
         print(new_letter)
+        with open(f"output/readytosend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
